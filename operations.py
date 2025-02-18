99be3438d82e84defdfd558ff7b9b14665e14a05
Nicolas Quintana <A01785655@tec.mx>
2025-02-18 12:46:21 -0600

    Added addition and subtraction.

--- operations.py
+++ operations.py
@@ -2,7 +2,9 @@ a = int(input("Give me the first number: "))
 b = int(input("Give me the second number: "))
 
 # Display the addition
+print("Addition: ", a + b)
 
 # Display the subtraction
+print("Subtraction: ", a - b)
 
 # Display multiplication
